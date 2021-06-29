7ab4866ebdfa298d903b695b03249f175a4f299c
Webb <webb.li@Kreakin.com>
2021-06-29 18:05:24 +0800

    add

---
+++ src/auth-provider.ts
@@ -0,0 +1,42 @@
+import { User } from "scrrens/project-list/search-panel";
+
+const localStorageKey = "__auth_provider_token__";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const getToken = () => window.localStorage.getItem(localStorageKey);
+
+export const handleUserResponse = ({ user }: { user: User }) => {
+  window.localStorage.setItem(localStorageKey, user.token || "");
+  return user;
+};
+
+export const login = (data: { username: string; password: string }) => {
+  fetch(`${apiUrl}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (res) => {
+    if (res.ok) {
+      handleUserResponse(await res.json());
+    }
+  });
+};
+
+export const register = (data: { username: string; password: string }) => {
+  fetch(`${apiUrl}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (res) => {
+    if (res.ok) {
+      handleUserResponse(await res.json());
+    }
+  });
+};
+
+export const logout = () => window.localStorage.removeItem(localStorageKey);
